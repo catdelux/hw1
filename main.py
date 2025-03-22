364169ec5a98007a7cf576c96921838947cf4ff1
chepuha777 <iliatrykisha@gmail.com>
2025-03-22 15:46:07 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-start = int(input("Введіть початкове число (з): "))
-end = int(input("Введіть кінцеве число (по): "))
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
-for num in range(start, end + 1):
-    print(num)
+    def get_year(self):
+        return f"{self.year}"
+    def get_model(self):
+        return f"{self.model}"
+    def get_make(self):
+        return f"{self.make}"        
+
+car1 = Car("Toyota", "Corolla", 2020)
+
+print(car1.get_year())
+print(car1.get_model())
+print(car1.get_make())
